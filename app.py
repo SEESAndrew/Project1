1d61b02fad4d014a98ea28e2bb66195cffeaedb2
SEESAndrew <drewlorello@yahoo.com>
2021-02-07 21:02:21 +0000

    Third Commit

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ import random
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+#Personal client credentials to access the API
 CLIENT_ID = 'ec2f029b0f0e4d30a67884900d9f53b6'
 CLIENT_SECRET = '7089726d89be40fcbf15f8e24b3751f5'
-AUTH_TK_URL = 'https://accounts.spotify.com/api/token'
 
-AUTH_RSP = requests.post(AUTH_TK_URL, {
+#Storing response from the server using POST
+AUTH_RSP = requests.post('https://accounts.spotify.com/api/token', {
     'grant_type':'client_credentials',
     'client_id':CLIENT_ID,
     'client_secret':CLIENT_SECRET
 })
 AUTH_RESPONSE = AUTH_RSP.json()
+
+#Formatting API requests to remove clutter and improve readability
 ACCESS_TK = AUTH_RESPONSE['access_token']
-
 headers = { 'Authorization': 'Bearer {token}'.format(token=ACCESS_TK) }
-
 BASE = 'https://api.spotify.com/v1/'
 
+#Chooses a random track from a list of recent favorites
 TRACK_ID_LIST = ['6i0V12jOa3mr6uu4WYhUBr', '1kd5qplldnxu16qcZXS3Yk', '4FHhXviyyftv946wV4I5P5', '3Wn52FjoUJClQOXwKePPp3', '0ytvsZOerGzUWfHXVT2Sgy']
 TRACK_ID = random.choice(TRACK_ID_LIST)
 
