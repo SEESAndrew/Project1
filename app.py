41aed5fb4e29775a5f708d222da79e15ac09c27a
SEESAndrew <drewlorello@yahoo.com>
2021-02-17 00:39:51 +0000

    Further cleaning up app.py

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 import requests
 import os
 import random
+from dotenv import load_dotenv, find_dotenv
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+load_dotenv(find_dotenv())
+
 #Personal client credentials to access the API
-CLIENT_ID = 'ec2f029b0f0e4d30a67884900d9f53b6'
-CLIENT_SECRET = '7089726d89be40fcbf15f8e24b3751f5'
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 #Storing response from the server using POST
 AUTH_RSP = requests.post('https://accounts.spotify.com/api/token', {
@@ -24,7 +27,7 @@ headers = { 'Authorization': 'Bearer {token}'.format(token=ACCESS_TK) }
 BASE_SPT = 'https://api.spotify.com/v1/'
 
 #Chooses a random track from a list of recent favorites
-TRACK_ID_LIST = ['1y4jsQt7MjnZhiD1L6qFBC', '1kd5qplldnxu16qcZXS3Yk', '5fbSIKNisMBlP1tXxjziJb', '6ZVuGZvrViwA5uliEQ4F7Y', '0ytvsZOerGzUWfHXVT2Sgy', '3yOlyBJuViE2YSGn3nVE1K']
+TRACK_ID_LIST = ['1y4jsQt7MjnZhiD1L6qFBC', '1kd5qplldnxu16qcZXS3Yk', '5fbSIKNisMBlP1tXxjziJb', '6ZVuGZvrViwA5uliEQ4F7Y', '3ZYN2cfyCFn4NuWxEW9tuh', '3yOlyBJuViE2YSGn3nVE1K']
 TRACK_ID = random.choice(TRACK_ID_LIST)
 
 @app.route('/')
@@ -32,7 +35,8 @@ def get_song_info():
     r = requests.get(BASE_SPT + 'tracks/' + TRACK_ID, headers=headers)
     request = r.json()
     genius_url = 'http://api.genius.com/search'
-    genius_header = {'Authorization': 'BEARER QW7LEM7QIeuuZlKcb4kWsy7Q9Z7ECUNKCsiRyvZNXICUi1a1758J_bDydLEOk5gx'}
+    GENIUS_TOKEN = os.getenv('GENIUS_TOKEN')
+    genius_header = {'Authorization': 'BEARER {token}'.format(token=GENIUS_TOKEN)}
     song_title = request['name']
     params = {'q': song_title}
     s = requests.get(genius_url, params=params, headers=genius_header)
@@ -42,7 +46,7 @@ def get_song_info():
         if hit['result']['primary_artist']['name'].lower() == request['album']['artists'][0]['name'].lower():
             song_lyrics_url = hit['result']['url']
             break
-    print(song_lyrics_url)
+    print(request)
     return render_template(
         "index.html",
         lyric_link = song_lyrics_url,
